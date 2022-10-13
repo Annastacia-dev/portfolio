5477b54c794e8d3481465172aa4f0fd26950bd86
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2022-10-13 22:19:28 +0300

    styling navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,37 +1,41 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import logo from '../images/bluelogo.png';
 import '../css/navbar.css'
 
 const NavBar = () => {
   return (
     <div>
       <nav>
-        <NavLink className="logo">
-          <img src={logo} alt="" style={{"width": "30px"}} />
-          <span>annastacia.</span>
+        <NavLink end to='/' className="logo">
+          <span>annastacia .</span>
         </NavLink>
         <div className="navbar">
           <ul>
             <li>
-                <NavLink>home</NavLink>
+                <NavLink end to='/'>home</NavLink>
             </li>
             <li>
-                <NavLink>about me</NavLink>
+                <NavLink end to='/about'>about me</NavLink>
             </li>
             <li>
-                <NavLink>portfolio</NavLink>
+                <NavLink end to='portfolio'>portfolio</NavLink>
             </li>
             <li>
                 <NavLink>
                   <select name="services" id="services">
-                    <option value="whatido" selected defaultValue hidden>what i do</option>
+                    <option value="whatido" defaultValue hidden>what i do</option>
+                    <NavLink></NavLink>
                     <option value="brand design"><NavLink>brand design</NavLink></option>
                     <option value="uiux"><NavLink>UI/UX</NavLink></option>
                     <option value="webdevelopment"><NavLink>web development</NavLink></option>
                     <option value="smm"><NavLink>social media management</NavLink></option>
                   </select>
                 </NavLink>
+            </li>
+            <li>
+              <button>
+                Get a quote
+              </button>
             </li>
           </ul>
         </div>
