75976953fc285ae727732b499f82c8e292a55a41
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2022-10-14 12:24:03 +0300

    initiate home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import NavBar from './components/NavBar';
+import Home from './components/Home';
 
 
 function App() {
   return (
     <div className="App">
         <NavBar/>
+        <Home/>
+
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../css/home.css'
+
+const Home = () => {
+  return (
+    <>
+    <div className='container'>
+    <h1>
+        Hey, I'm Annastacia 👋
+        <br/> I like to
+        <span id="typewriter"></span>
+        <span id="cursor">|</span>
+    </h1>
+    </div>
+      
+    </>
+  )
+}
+
+export default Home
+
