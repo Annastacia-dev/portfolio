3085dfbdcc0812028840b7a94ec2661ef234c294
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2022-10-17 20:47:40 +0300

    initialize portfolio

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar';
 import Home from './components/Home';
 import About from './components/About'
 import Services from './components/Services';
+import Portfolio from './components/Portfolio';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Home/>
         <About/>
         <Services />
+        <Portfolio />
 
     </div>
   );
---
+++ src/components/Portfolio.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Portfolio = () => {
+  return (
+    <div className='container portfolio'>
+        <div className='content portfolio'>
+            <h1> Portfolio </h1>
+        </div>
+       
+    </div>
+  )
+}
+
+export default Portfolio
