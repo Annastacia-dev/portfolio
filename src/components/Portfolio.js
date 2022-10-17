3eb933308060f357db2ed52206d52f7f821aa450
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2022-10-17 21:05:22 +0300

    changes

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import '../css/portfolio.css'
 
 const Portfolio = () => {
   return (
     <div className='container portfolio'>
         <div className='content portfolio'>
-            <h1> Portfolio </h1>
+            <h1> Show Case</h1>
+            <div className="description portfolio">
+                <p>case studies of my projects</p>
+            </div>
         </div>
        
     </div>
--- src/components/Services.js
+++ src/components/Services.js
@@ -10,21 +10,14 @@ const Services = () => {
   return (
     <div className='container services'>
         <div className="content services">
-            <h1>
-                What I do
-            </h1>
+            <h1>What I do</h1>
             <div className="description services">
-                <p>
-                    My creative expertise
-                </p>
-
+                <p>My creative expertise</p>
                 <div className='cards-container'>
                 {services.map(service => <Card key={service.id} service={service} />)}
                 </div>
-
             </div>
         </div>
-
     </div>
   )
 }
