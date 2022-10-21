fbcec7de24cdcccccde29dce35f7766ef6ff46eb
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2022-10-21 20:53:51 +0300

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,26 +7,49 @@ import aid from '../images/aid.svg'
 import pr from '../images/pr.svg'
 import an from '../images/an.svg'
 import fi from '../images/fi.svg'
+import html from '../images/html.svg'
+import css from '../images/css.png'
+import ruby from '../images/ruby.png'
+import js from '../images/js.png'
+import react from '../images/react.svg'
+import rr from '../images/rr.svg'
+
 
 const Footer = () => {
   return (
     <div className='container footer'>
         <div className="content footer">
-        <h1>Footer</h1>
+        <h1>Softwares &amp; Stacks</h1>
         <div className="description footer">
-        <h3>Softwares &amp; Stacks</h3>
-            <div className="category">
+        <div className="skills">
+            <div className="category softwares">
             <img src={ai} alt="" style={{"width":"100px"}} />
-            <img src={ps} alt="" style={{"width":"50px", "marginBottom":"8px"}} />
+            <img src={ps} alt="" style={{"width":"50px", "marginBottom":"8px","marginLeft":"10px","marginRight":"10px"}} />
             <img src={ae} alt="" style={{"width":"100px"}} />
             <img src={aid} alt="" style={{"width":"100px"}} />
             <img src={pr} alt="" style={{"width":"100px"}} />
             <img src={an} alt="" style={{"width":"100px"}} />
-            <img src={fi} alt="" style={{"width":"30px"}} />
+            <img src={fi} alt="" style={{"width":"30px", "marginBottom":"10px","marginLeft":"10px",
+            "marginRight":"10px"}} />
             </div>
-            
-           
+            <div className="category stacks">
+            <img src={html} alt="" style={{"width":"40px","marginBottom":"10px","marginLeft":"25px","marginRight":"25px"}} />
+            <img src={css} alt="" style={{"width":"45px","marginLeft":"25px","marginRight":"25px","marginBottom":"10px"}} />
+            <img src={js} alt="" style={{"width":"45px","marginLeft":"15px","marginRight":"25px","marginBottom":"10px"}} />
+            <img src={ruby} alt="" style={{"width":"40px","marginLeft":"35px","marginRight":"25px","marginBottom":"10px"}} />
+            <img src={react} alt="" style={{"width":"100px",}} />
+            <img src={rr} alt="" style={{"width":"100px","marginLeft":"15px"}} />
+            </div>  
+            </div>
         </div>
+         <div className="socials">
+         <h1>Lets connect</h1>
+         <div className="description socials">
+
+         </div>
+         </div>
+        
+      
         
         </div>
     </div>
